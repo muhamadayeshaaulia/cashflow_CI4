6559f45a90546fd3f1233efa93788805db23cd6a
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-22 20:27:16 +0700

    membuat field untuk kebutuhan kas masuk

--- app/Database/Migrations/2026-03-22-132147_KasMasuk.php
+++ app/Database/Migrations/2026-03-22-132147_KasMasuk.php
@@ -8,11 +8,60 @@ class KasMasuk extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'tanggal_pembuatan' => [
+                'type' => 'DATE',
+            ],
+            'no_bast' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => true,
+            ],
+            'no_tagihan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => true,
+            ],
+            'no_sp2d' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => true,
+            ],
+            'nominal' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '15,2',
+            ],
+            'status' => [
+                'type'       => 'ENUM',
+                'constraint' => ['proses_kirim', 'tagihan_dikirim', 'sp2d_terbit', 'lunas'],
+                'default'    => 'proses_kirim',
+            ],
+            'bukti_transfer' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('kas_masuk');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('kas_masuk');
     }
 }
