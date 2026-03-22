c87f2326df9c52394902dc2fda34059ac2ad9988
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-22 20:32:12 +0700

    membuat data dummy yg akan di masukan kedatabase untuk tahap awal

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,6 +8,32 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $data = [
+            [
+                'username'   => 'purchasing',
+                // gunakan password_hash agar aman
+                'password'   => password_hash('123456', PASSWORD_DEFAULT),
+                'role'       => 'purchasing',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'username'   => 'manajer',
+                'password'   => password_hash('123456', PASSWORD_DEFAULT),
+                'role'       => 'manajer',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'username'   => 'admin_keuangan',
+                'password'   => password_hash('123456', PASSWORD_DEFAULT),
+                'role'       => 'admin_keuangan',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        // Masukkan data ke dalam tabel 'users'
+        $this->db->table('users')->insertBatch($data);
     }
 }
