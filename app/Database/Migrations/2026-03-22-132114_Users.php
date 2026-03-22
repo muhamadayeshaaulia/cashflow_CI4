f26b9f0b002bc39c06685235b44c6e039081f8a7
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-22 20:24:37 +0700

    membuat field untuk kebutuhan tabel users sementara

--- app/Database/Migrations/2026-03-22-132114_Users.php
+++ app/Database/Migrations/2026-03-22-132114_Users.php
@@ -8,11 +8,40 @@ class Users extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'role' => [
+                'type'       => 'ENUM',
+                'constraint' => ['purchasing', 'manajer', 'admin_keuangan'],
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
