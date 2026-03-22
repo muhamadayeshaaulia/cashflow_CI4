b5fd7711c26c10e9fe0a815b495925ca508e9096
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-23 03:36:49 +0700

    menambahkan user mode di controller auth

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\UserModel;
 
 class AuthController extends BaseController
 {
