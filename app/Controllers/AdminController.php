2bb49b66e834f805cc69343e67d3846af04be654
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-23 03:42:55 +0700

    menambahkan logika sementara untuk admin controller

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -7,8 +7,16 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class AdminController extends BaseController
 {
-    public function index()
+   public function index()
     {
-        //
+        if (session()->get('role') !== 'admin_keuangan') {
+            return redirect()->to('/login');
+        }
+
+        $data = [
+            'title' => 'Dashboard Admin Keuangan'
+        ];
+        
+        return view('admin/dashboard', $data);
     }
 }
